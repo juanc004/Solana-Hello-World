ccdccd1c918fdfcd938da91d63011403cf509802
juanc004 <124308226+juanc004@users.noreply.github.com>
2024-05-31 16:05:36 -0500

    first commit

---
+++ programs/solana-test/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("6fnhTz8Y8QNyLy5cjge8QvdyiLw3PPSBxDKuQg5NyUPJ");
+
+#[program]
+pub mod solana_test {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
